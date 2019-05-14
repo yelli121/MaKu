67aef57b615452694201177ee43e8998abe522b9
muhamadfahriza <48055788+muhamadfahriza@users.noreply.github.com>
2019-05-14 22:04:13 +0700

    javadoc balance, database balance dan Exception

--- java/BalanceAlreadyExistsException.java
+++ java/BalanceAlreadyExistsException.java
@@ -1,16 +1,24 @@
 
 /**
- * Write a description of class BalanceAlreadyExistsException here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * Mengidikasikan balance sudah ada/digunakan
+ * 
+ * @author Muhamad Fahriza Novriansyah
+ * @version 1.0
  */
 public class BalanceAlreadyExistsException extends Exception{
     private Balance balance_error;
-
+    /**
+     * Konstruktor untuk objek kelas BalanceAlreadyExistsException
+     * @param balance_input balance yang sudah ada                      
+     */
     public BalanceAlreadyExistsException(Balance balance_input) {super("balance : ");
-        balance_error = balance_input;}
-
+        balance_error = balance_input;
+    }
+        
+    /**
+     * Untuk menampilkan pesan pemberitahuan berupa String
+     * @return balance sudah ada/digunakan                    
+     */
     public String getExMessage(){ return super.getMessage() + "Balance"+
            " already exist";}
 }
